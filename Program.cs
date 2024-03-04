d88e405a9dcadb8c452821d8b04b5e6283970f68
TEJAS <tejasgowda0524@gmail.com>
2024-03-04 22:38:18 +0530

    added

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace uc4
+{
+    public class moodAnalyser
+    {
+
+        public string message;
+
+        public moodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public moodAnalyser()
+        {
+            
+            this.message = message;
+        }
+        public void analyseMood(string message)
+        {
+
+            try
+            {
+                
+                if (message != null)
+                {
+                    string[] m = message.Split(' ');
+
+                    if (m.Contains("sad"))
+                    {
+                        Console.WriteLine("SAD :(");
+                    }
+                    else
+                    {
+                        Console.WriteLine("HAPPY :)");
+                    }
+                }
+                else
+                {
+                    throw new moodAnalysisException("Null reference exception occurred..");
+                    //Console.WriteLine("Null reference exception occurred..");
+                }
+            }
+            catch (moodAnalysisException e)
+            {
+                Console.WriteLine("exception occurred: " +e.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+               // moodAnalyser ma1 = new moodAnalyser("im happy");
+               moodAnalyser ma = new moodAnalyser();
+                // moodAnalyser ma = new moodAnalyser(null);
+
+
+                Console.WriteLine("enter your current mood ..");
+                string mood = Console.ReadLine();
+
+
+                // string mood = null;
+               // ma1.analyseMood();
+                
+                ma.analyseMood(mood);
+
+            }
+        }
+    }
+}
+    
---
+++ exception.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uc4
+{
+    public class moodAnalysisException : Exception
+    {
+        public moodAnalysisException(string s) : base(s)
+        {
+
+        }
+    }
+}
